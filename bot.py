4c908be19d9a5f72bb229b66f4e0eee01f9bcea4
Rachelle <rachellecha@users.noreply.github.com>
2020-11-07 12:18:53 -0500

    basical on messasge and command prefix

---
+++ bot.py
@@ -0,0 +1,86 @@
+# bot.py
+import os
+import random
+
+import discord
+from dotenv import load_dotenv
+from discord.ext import commands
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+#client = discord.Client()
+bot = commands.Bot(command_prefix='!')
+
+#commands
+
+@bot.command(name='99', help='Responds with a random quote from Brooklyn 99')
+async def nine_nine(ctx):
+    print(ctx)
+    brooklyn_99_quotes = [
+        'I\'m the human form of the 💯 emoji.',
+        'Bingpot!',
+        (
+            'Cool. Cool cool cool cool cool cool cool, '
+            'no doubt no doubt no doubt no doubt.'
+        ),
+    ]
+
+    response = random.choice(brooklyn_99_quotes)
+    await ctx.send(response)
+    
+'''
+@bot.event
+async def on_ready():
+    for guild in bot.guilds:
+        if guild.name == GUILD:
+            break
+
+    print(
+        f'{bot.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})\n'
+    )
+
+    members = '\n - '.join([member.name for member in guild.members])
+    print(f'Guild Members:\n - {members}')
+
+'''
+
+#event responses
+@bot.event
+async def on_message(message):
+    await bot.process_commands(message)
+
+    if message.author == bot.user:
+        return
+
+    #add more quotes!
+    fail_quote = [
+        "Don't give up!",
+        "Failure is only the opportunity to begin again more intelligently.",
+        "You can always bounce back!",
+        "Failure is an attitude, not an outcome."
+    ]
+
+    tired_quote = [
+        "She's strong but she's exhausted.",
+        "I know you are tired, but you have to keep going!",
+        "The more you sweat in practice, the less you bleed in battle.",
+        "WAKE UP!!"
+    ]
+
+    if 'failed' in message.content:
+        response = random.choice(fail_quote)
+        await message.channel.send(response)
+    
+    if 'tired' in message.content:
+        response = random.choice(tired_quote)
+        await message.channel.send(response)
+    
+        
+
+
+bot.run(TOKEN)
+
+#client.run(TOKEN)
